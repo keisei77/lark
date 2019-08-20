0117e5fd71b1951aa6442e74cf5a0d91938a0fb8
Keisei Jia <keisei.jia@qq.com>
2019-08-20 23:30:20 +0800

    :construction: add preliminary prototype

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { isElementVisible } from './utils/isElementVisible';
+
 function lark(el: Element | string | null) {
   // assume el is a HTMLElement
   if (typeof el === 'string') {
+    // TODO: need more detailed handle
     el = document.getElementById(el) || document.querySelector(el);
     if (!el) {
       return;
@@ -8,4 +11,11 @@ function lark(el: Element | string | null) {
   }
 
   const elProperty = el!.getBoundingClientRect();
+  const isElVisible = isElementVisible(elProperty);
+
+  // Then we can put a floating element at right top corner inside the given element.
+
+  return function floating(floatingElement: Element) {
+    (el as Element).prepend(floatingElement);
+  };
 }
